4af5c3a123bdd087f0f5b8a4369dc4919251d811
Sajjad Javanmardi <sajjaad@gmail.com>
2021-05-27 18:34:56 +0200

    fixed font url

--- farsifonts.sh
+++ farsifonts.sh
@@ -13,7 +13,7 @@ function detect(){
 }
 
 # Address of toc file, I keep this file updated.
-readonly URL="http://fzerorubigd.github.com/persian-fonts-linux/list"
+readonly URL="https://raw.githubusercontent.com/fzerorubigd/persian-fonts-linux/gh-pages/list"
 readonly TOC="/tmp/list.txt"
 
 if [ $# -eq 0 ]; then
