36fd5ecbb39917d66da194031509448befb88f91
Eshagh <ase.mugem@gmail.com>
2021-07-08 10:29:32 +0430

    Update farsifonts.sh

--- farsifonts.sh
+++ farsifonts.sh
@@ -7,7 +7,7 @@
 # You can use "axel" instead of default "wget", just install axel first and call this
 # with axel parameter.
 
-FONT_PATH="$HOME/.fonts/truetype/"
+FONT_PATH="$HOME/.local/share/fonts/"
 # Address of toc file, I keep this file updated.
 readonly URL="http://fzero.rubi.gd/persian-fonts-linux/list"
 readonly TOC="/tmp/list.txt"
